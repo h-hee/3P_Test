70c893a13ca0ae3550638b08f1c5cf61722d092c
ㅎㅎ <USER@192.168.56.1>
2018-02-04 16:00:28 +0900

    0204 컨트롤러 생성

---
+++ 3P_Test/src/common/controller/CommonController.java
@@ -0,0 +1,10 @@
+package common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/common")
+public class CommonController {
+
+}
---
+++ 3P_Test/src/member/controller/MemberController.java
@@ -0,0 +1,17 @@
+package member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	
+	// 화면 띄우기
+	@RequestMapping(value="/{url}.do")
+	public String url(@PathVariable String url) {
+		return "/member/" + url;
+	}
+}
