271200991191cb2a109c597acb6095454ecde4cc
ㅎㅎ <USER@192.168.56.1>
2018-02-04 18:03:02 +0900

    0204 컨트롤러 수정, index수정, servlet 수정

--- 3P_Test/src/member/controller/MemberController.java
+++ 3P_Test/src/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/member")
@@ -14,4 +15,5 @@ public class MemberController {
 	public String url(@PathVariable String url) {
 		return "/member/" + url;
 	}
+
 }
