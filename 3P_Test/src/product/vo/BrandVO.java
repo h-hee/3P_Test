52264b3efb40f435ab967d2a10d614bdf8e00fc3
신승용 <@218.159.219.10>
2018-02-12 06:13:18 +0900

    18.02.11	신승용		commit&push
    product		vo			BrandVO								update
    						PerfumeVO							update

---
+++ 3P_Test/src/product/vo/BrandVO.java
@@ -0,0 +1,23 @@
+package product.vo;
+
+public class BrandVO {
+	private int		bdSq;
+	private String 	bdName;
+	
+	
+	public int getBdSq() {
+		return bdSq;
+	}
+	public void setBdSq(int bdSq) {
+		this.bdSq = bdSq;
+	}
+	public String getBdName() {
+		return bdName;
+	}
+	public void setBdName(String bdName) {
+		this.bdName = bdName;
+	}
+	
+	
+	
+}
---
+++ 3P_Test/src/product/vo/PerfumeVO.java
@@ -0,0 +1,54 @@
+package product.vo;
+
+public class PerfumeVO {
+	private int		perSq;
+	private int 	bdSq;
+	private String 	perName;
+	private int		perPrice;
+	private String	perShort;
+	private String	perLong;
+	private String 	perDate;
+	
+	public int getPerSq() {
+		return perSq;
+	}
+	public void setPerSq(int perSq) {
+		this.perSq = perSq;
+	}
+	public int getBdSq() {
+		return bdSq;
+	}
+	public void setBdSq(int bdSq) {
+		this.bdSq = bdSq;
+	}
+	public String getPerName() {
+		return perName;
+	}
+	public void setPerName(String perName) {
+		this.perName = perName;
+	}
+	public int getPerPrice() {
+		return perPrice;
+	}
+	public void setPerPrice(int perPrice) {
+		this.perPrice = perPrice;
+	}
+	public String getPerShort() {
+		return perShort;
+	}
+	public void setPerShort(String perShort) {
+		this.perShort = perShort;
+	}
+	public String getPerLong() {
+		return perLong;
+	}
+	public void setPerLong(String perLong) {
+		this.perLong = perLong;
+	}
+	public String getPerDate() {
+		return perDate;
+	}
+	public void setPerDate(String perDate) {
+		this.perDate = perDate;
+	}
+}
